ba8e71c7cae6065ecc8ca2617e7b8c4089310b0c
Harsh Tiwari <harshtiwari343@email.com>
2024-09-19 09:52:49 +0530

    added comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // builtin module
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // root id is defined inside index.html file
 root.render(
   "Programming with Harsh"
 );
