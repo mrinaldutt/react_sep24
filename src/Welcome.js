f7af3d35a991a7f09ef98cfed2a578e294a77ac0
Harsh Tiwari <harshtiwari343@email.com>
2024-09-20 12:06:42 +0530

    Added Bootstrap Spinner and Button Class

--- src/Welcome.js
+++ src/Welcome.js
@@ -24,10 +24,18 @@ export default class Welcome extends React.Component {
             textAlign: "center"
         }
         return <>
+            <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+
+            <div className="spinner-grow" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+
             <h1 style={myStyle}>Welcome Class Component</h1>
             <h2 className='header'>External CSS Example</h2>
             <h2 style={{ backgroundColor: "yellow" }}>State Example - username is {this.state.username}</h2>
-            <button onClick={this.updateUsername}>Update Username</button>
+            <button className="btn btn-outline-primary" onClick={this.updateUsername}>Update Username</button>
             <h3>Props Example - He is having mobile {this.props.mobileProps}</h3>
         </>
     }
